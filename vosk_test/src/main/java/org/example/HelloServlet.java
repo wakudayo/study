7191a222e9bf576abe2c430b10f711db7da71581
taijin waku <xaviers916@icloud.com>
2025-03-25 18:06:13 +0900

    voskをjavaでつかう 初回コミット

---
+++ vosk_test/src/main/java/org/example/HelloServlet.java
@@ -0,0 +1,17 @@
+package org.example;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/hello")
+public class HelloServlet extends HttpServlet {
+  protected void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+    res.setContentType("text/plain");
+    res.getWriter().write("Hello World!");
+  }
+}
